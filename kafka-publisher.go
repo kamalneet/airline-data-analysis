ee3553fdc661bf8465d9e0aaa4ecf7dcd2940356
Kamalneet Singh <kamalneet@gmail.com>
2017-09-20 09:58:36 +0000

    Published all files to kafka
    ~/go/bin/publish_to_kafka /v2/clean-data

--- kafka-publisher.go
+++ kafka-publisher.go
@@ -16,15 +16,22 @@ func publishRecordToKafka(line string) {
   topic := "airline"
 	err := kafkaProducer.Produce(&kafka.Message{TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny}, Value: []byte(line)}, kafkaDeliveryChannel)
   check(err, line)
-	e := <-kafkaDeliveryChannel
-	m := e.(*kafka.Message)
+}
 
-	if m.TopicPartition.Error != nil {
-		log.Fatal("Delivery failed:", m.TopicPartition.Error)
-	} else {
-		fmt.Printf("Delivered message to topic %s [%d] at offset %v\n",
-			*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
-	}
+func kafkaVerifyDelivery(n int) {
+  var lastOffset kafka.Offset
+  for i := 0; i < n; i++ {
+    e := <-kafkaDeliveryChannel
+    m := e.(*kafka.Message)
+    if m.TopicPartition.Error != nil {
+      log.Fatal("Delivery failed:", m.TopicPartition.Error)
+    } else {
+      lastOffset = m.TopicPartition.Offset
+    }
+  }
+  _ = lastOffset
+ // log.Println("Verified delivery of", n, "messages, upto offset", lastOffset)
+  fmt.Printf(".")
 }
 
 func publishFileToKafka(file string) {
@@ -40,12 +47,23 @@ func publishFileToKafka(file string) {
   if !strings.HasPrefix(header, "Year,") {
     log.Fatal("Unexpected header", header, "in file", file)
   }
+  kafkaNumOutstanding := 0
   for scanner.Scan() {
     publishRecordToKafka(scanner.Text())
+    kafkaNumOutstanding++
+    if kafkaNumOutstanding > 127 {
+      kafkaVerifyDelivery(kafkaNumOutstanding)
+      kafkaNumOutstanding = 0
+    }
   }
   if err := scanner.Err(); err != nil {
     log.Fatal("Error reading", file, err)
   }
+  if kafkaNumOutstanding > 0 {
+    kafkaVerifyDelivery(kafkaNumOutstanding)
+    kafkaNumOutstanding = 0
+  }
+  fmt.Println()
 }
 
 func kafka_publish_main() {
@@ -64,6 +82,7 @@ func kafka_publish_main() {
 	kafkaDeliveryChannel = make(chan kafka.Event)
 
   for _, csv_file := range csv_files {
+    fmt.Printf(csv_file)
     publishFileToKafka(csv_file)
   }
 
