bf563f7daea84cdd0bca3233376bd57068786919
Kamalneet Singh <kamalneet@gmail.com>
2017-10-14 14:45:00 +0000

    Spark can do MR. Do airport popularity.
    Verified with a single csv file

--- py/main.py
+++ py/main.py
@@ -14,33 +14,37 @@ def getFieldIndex(field):
   for f in csvFields:
     if f == field:
 			return i
-    i++
+    i+=1
   return -1
 
 def airportMapper(csv):
-  toks=l.split(",")
+#  print(csv)
+  toks=csv[1].split(",")
   if toks[0] == "Year":
     return []
   src_idx = getFieldIndex("Origin")
   dst_idx = getFieldIndex("Dest")
   return [(toks[src_idx],1), (toks[dst_idx],1)]
 
-def createContext(host, port, outputPath):
+def countReducer(newvals, v2):
+#  print("values: " + str(v1) + " " + str(v2))
+  return sum(newvals) + (v2 or 0)
+
+def createContext():
     # If you do not see this printed, that means the StreamingContext has been loaded
     # from the new checkpoint
     print("Creating new context")
-    if os.path.exists(outputPath):
-        os.remove(outputPath)
     sc = SparkContext(appName="AirlineDataAnalysis")
     ssc = StreamingContext(sc, 1)
     
-    csvStream = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers, "auto.offset.reset": "smallest"})
-    aptCounts = words.map(airportMapper).reduceByKey(lambda x, y: x + y)
+    csvStream = KafkaUtils.createDirectStream(ssc, ["atest"], {"metadata.broker.list": "172.31.81.70:9092", "auto.offset.reset": "smallest"})
+    aptCounts = csvStream.flatMap(airportMapper).updateStateByKey(countReducer)
     aptCounts.pprint()
+    aptCounts.saveAsTextFiles("apt_counts")
     return ssc
 
 if __name__ == "__main__":
     ssc = StreamingContext.getOrCreate("/home/centos/spark-checkpoint",
-                                       lambda: createContext(host, int(port), output))
+                                       lambda: createContext())
     ssc.start()
     ssc.awaitTermination()
